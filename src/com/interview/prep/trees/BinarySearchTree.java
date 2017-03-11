ebb252087fe7a55517828c1ca0e400f13e602076
Nikitash Pawar <pawar.nikitash@gmail.com>
2017-03-11 16:12:42 +0530

    1 . Height of the Tree 2.  Check if tree is balanced 3.  Print a Tree

--- src/com/interview/prep/trees/BinarySearchTree.java
+++ src/com/interview/prep/trees/BinarySearchTree.java
@@ -44,6 +44,56 @@ public class BinarySearchTree {
         bst.insert(8);
         bst.insert(4);
         bst.printInOrder();
+        System.out.println("Height Of the Tree: "+heightOfTheTree(bst));
+        if(isBalancedTree(bst)){
+            System.out.println("Is Balanced");
+        }else {
+            System.out.println("Not Balanced");
+        }
+        BTreePrinter printer=new BTreePrinter();
+        printer.printNode(bst);
     }
+
+    public static int heightOfTheTree(Node node){
+        //height of the tree with a single node is 0
+        //while height of the tree with no node is -1
+        if (node==null){
+            return -1;
+        }
+
+        int leftHeight=heightOfTheTree(node.left);
+        int rightHeight=heightOfTheTree(node.right);
+
+        //if there is only root node then -1 is nullified
+        //with +1 and 0 is returned
+        if(leftHeight>rightHeight){
+            return leftHeight+1;
+        }else{
+            return rightHeight+1;
+        }
+    }
+
+    /**
+     * A Tree is Balanced If:
+     * 1. If the difference between the height of left subtree and
+     * the right subtree is not more than 1
+     * 2. Right subtree is balanced
+     * 3. Left subtree is balanced
+     * @param node
+     * @return
+     */
+    public static boolean isBalancedTree(Node node){
+
+        int leftHeight = heightOfTheTree(node.left);
+        int rightHeight = heightOfTheTree(node.right);
+
+        if(Math.abs(leftHeight-rightHeight)<=1 && isBalancedTree(node.left)&&isBalancedTree(node.right)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 
 }
--- src/com/interview/prep/trees/Node.java
+++ src/com/interview/prep/trees/Node.java
@@ -5,11 +5,11 @@ package com.interview.prep.trees;
  */
 public class Node {
 
-    private Node left,right;
+    Node left,right;
 
-    private Node parent;
+    Node parent;
 
-    private int data;
+    int data;
 
     public Node(int data){
         this.data=data;
@@ -95,59 +95,70 @@ public class Node {
         System.out.println("["+data+"] ");
     }
 
-    public void maxValue()
+    public int maxValue()
     {
         if(right!=null){
-            right.maxValue();
+            return right.maxValue();
         }else {
             System.out.println("Max Val :"+data);
+            return data;
         }
     }
 
-    public void minValue()
+    public int minValue()
     {
         if(left!=null){
-            left.minValue();
+            return left.minValue();
         }else {
             System.out.println("Min Val :"+data);
+            return data;
         }
     }
 
-    public void successorOf(int value){
+    public int successorOf(int value){
         Node node = find(value);
 
         if (node==null){
             System.out.println("Value does not exist");
-            return;
+            return value;
         }
 
-        //check if the node is left node of its parent
-        if(node.parent.left.data==node.data){
-            System.out.println("Successor of :"+node.data+" is:"+node.parent.data);
-            return;
+        if(node.right!=null){
+            // check if there is a right node for the left child
+            // as this will be greater than left node but less than the
+            //root node
+            System.out.println("Predecessor of :"+node.data+" is:");
+            return node.minValue();
         }
 
-        //check if the parent of the node doesnt have a left node
-        //if so then recurse upwards
-        while (node.parent!=null && node.parent.left==null){
-            node=node.parent;
-        }
-        if(node.parent==null){
-            System.out.println("No Successor as this is the Largest value in the Tree");
-            return;
-        }
-        System.out.println("Successor is :"+node.data);
+        Node parent=node.parent;
 
+        while (parent!=null && node==parent.right){
+        node=parent;
+        parent=parent.parent;
+        }
+
+        System.out.println("Successor is :"+parent.data);
+        return parent.data;
     }
 
-    public void predecessorOf(int value){
+    public int predecessorOf(int value){
         Node node = find(value);
         Node parent;
 
         if(node.left!=null){
+            // check if there is a right node for the left child
+            // as this will be greater than left node but less than the
+            //root node
             System.out.println("Predecessor of :"+node.data+" is:");
-            node.maxValue();
+            return node.maxValue();
         }
+        /*
+        if there is no left node then go to parent
+        then go to parents parent
+        then check if it has a left node
+        once you find the left node print it
+        */
         parent=node.parent;
         while (parent!=null && node==parent.left ){
             node=parent;
@@ -155,5 +166,114 @@ public class Node {
         }
         System.out.println("Predecessor is :"+parent.data);
 
+        return parent.data;
     }
+
+    public void delete(Node node,int value)
+    {
+        /**
+         * If V is present delete it
+         * If deleted node is  a leaf node then done
+         * If deleted node has only one child then promote it
+         * If deleted node has two children then  fill the hole
+         * with Predecessor or Successor
+         *      -then delete the pred or succssor
+         *      -Either leaf or only 1 child
+         */
+
+        if(node==null)
+        {
+            System.out.println("Invalid Node");
+            return;
+        }
+
+        //recursive cases
+        if(value < node.data)
+        {
+            if (node.left!=null){
+                delete(node.left,value);
+            }
+            return;
+        }
+
+        if(value > node.data)
+        {
+            if (node.right!=null){
+                delete(node.right,value);
+            }
+            return;
+        }
+
+        //lets say value is found in the tree then delete it
+        //as per following cases
+
+        if(value==node.data){
+
+            //case 1: node is a root node
+            //set the node to null and return
+            if(node.parent==null){
+                node=null;
+                return;
+            }
+
+            //case 2: node is a leaf node
+            //check if right and left nodes are null and then
+            //find the parent
+            //check if the current node is left or right of the parent
+            //once identified set parents left or right node to null
+            if(node.left==null && node.right==null){
+                if(node.parent.left==node){
+                    node.parent.left=null;
+                }
+                else if(node.parent.right==node){
+                    node.parent.right=null;
+                }
+                return;
+            }
+
+            //case 3: node has only 1 child
+            //case 3.1: node has only a left child
+            // 1. make the nodes child point to the nodes parent
+            // 2. if the node is its parent's right then set the nodes
+            // child as the node's parents right else set it as the
+            // node's parent's left
+            if(node.left!=null && node.right==null){
+                node.left.parent=node.parent;
+                if(node.parent.left==node){
+                    node.parent.left=node.left;
+                }else{
+                    node.parent.right=node.left;
+                }
+            return;
+            }
+
+            //case 3.2: node has only a right child
+            // 1. make the nodes child point to the nodes parent
+            // 2. if the node is its parent's right then set the nodes
+            // child as the node's parents right else set it as the
+            // node's parent's left
+            if(node.right!=null && node.left==null)
+            {
+                node.right.parent=node.parent;
+                if(node.parent.left==node){
+                    node.parent.left=node.right;
+                }else{
+                    node.parent.right=node.right;
+                }
+            return;
+            }
+
+            //case 4: node has two children
+            //Copy the predecessor into the current node
+            //Delete the predecessor form the left subtree
+            //Predecessor either a leaf of a single child
+
+            int predecessorValue=predecessorOf(value);
+            node.data=predecessorValue;
+            delete(node.left,predecessorValue);
+
+        }
+     }
+
+
 }
